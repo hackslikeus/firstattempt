231056bef8ece60b3aae3fe453d1b3440ce4487e
gary kissell <garykissell@yahoo.com>
2019-12-01 22:08:28 -0600

    Update test2a.py

--- test2a.py
+++ test2a.py
@@ -9,5 +9,7 @@ btngray_choice = random.choice(btngray)
 
 print ("Randomly selected item from list is ", btngray_choice)
 
-subprocess.call(['mpg123', btngray_choice])
+#remove meta tag feed and added buffer to reduce pop
+#subprocess.call(['mpg123', btngray_choice])
+subprocess.call(['mpg123', '-q', btnblue_choice, " --preload 1"])
 
