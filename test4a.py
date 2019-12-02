5fc25ff930094522dfc832126ef9c43c4d5ff413
gary kissell <garykissell@yahoo.com>
2019-12-01 22:16:50 -0600

    Update test4a.py
    better list formatting and reduced meta tag feed and added pop suppression with buffer

--- test4a.py
+++ test4a.py
@@ -3,10 +3,19 @@ import sys
 import random
 import subprocess
 
-btnoran = ['http://centreforce.streamgb.com:8830/stream', 'http://ice4.somafm.com/bagel-128-mp3', 'http://ice4.somafm.com/poptron-128-mp3', 'http://sharprouter.sharp-stream.com:8000/nme1high.mp3', 'http://sharprouter.sharp-stream.com:8000/nme2high.mp3','http://ice2.somafm.com/u80s-256-mp3', 'http://ice2.somafm.com/indiepop-128-mp3',  'http://149.56.195.94:8056/stream', 'http://media-ice.musicradio.com:80/RadioXLondonMP3']
+btnoran = ['http://centreforce.streamgb.com:8830/stream', 
+           'http://ice4.somafm.com/bagel-128-mp3', 
+           'http://ice4.somafm.com/poptron-128-mp3', 
+           'http://sharprouter.sharp-stream.com:8000/nme1high.mp3', 
+           'http://sharprouter.sharp-stream.com:8000/nme2high.mp3',
+           'http://ice2.somafm.com/u80s-256-mp3', 
+           'http://ice2.somafm.com/indiepop-128-mp3',  
+           'http://149.56.195.94:8056/stream', 
+           'http://media-ice.musicradio.com:80/RadioXLondonMP3']
 
 btnoran_choice = random.choice(btnoran)
 
 print ("And The Winner is:  ", btnoran_choice)
 
-subprocess.call(['mpg123', '-q', btnoran_choice])
+#subprocess.call(['mpg123', '-q', btnoran_choice])
+subprocess.call(['mpg123', '-q', btnoran_choice, " --preload 1"])
